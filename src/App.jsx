f67e27c2b15264dfd99bf3db30820737c87426a2
JiHyoung You <fbwlgud51@gmail.com>
2026-04-21 03:18:06 +0900

    feat: analytics 위치 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { Analytics } from "@vercel/analytics/react";
 import "./App.css";
 import AppRouter from "./routes/AppRouter";
 
 function App() {
-  return <AppRouter />;
+  return (
+    <>
+      <AppRouter />
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -2,14 +2,12 @@ import { Box } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router";
 import Header from "./components/Header";
-import { Analytics } from "@vercel/analytics/react";
 
 const AppLayout = () => {
   return (
     <Box>
       <Header />
       <Outlet />
-      <Analytics />
     </Box>
   );
 };
