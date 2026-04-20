ed12e50d2088ea12dc13c41e99163d946df2046a
JiHyoung You <fbwlgud51@gmail.com>
2026-04-21 03:15:54 +0900

    feat: add vercel analytics

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -2,12 +2,14 @@ import { Box } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router";
 import Header from "./components/Header";
+import { Analytics } from "@vercel/analytics/react";
 
 const AppLayout = () => {
   return (
     <Box>
       <Header />
       <Outlet />
+      <Analytics />
     </Box>
   );
 };
