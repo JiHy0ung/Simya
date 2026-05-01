593016ae43f813db3a30942aea103de422e6dd72
JiHyoung You <fbwlgud51@gmail.com>
2026-05-01 19:13:26 +0900

    chore: 잼&와인 가격 패치 저굥ㅇ

--- src/constants/food/jam.js
+++ src/constants/food/jam.js
@@ -14,13 +14,13 @@ export const jamRecipes = [
   {
     name: "바나나 잼",
     image: BananaJam,
-    price: 21120,
+    price: 33120,
     ingredients: [{ name: "바나나", count: 64, image: getItemImage("바나나") }],
   },
   {
     name: "블루베리 잼",
     image: BlueberryJam,
-    price: 14080,
+    price: 22080,
     ingredients: [
       { name: "블루베리", count: 64, image: getItemImage("블루베리") },
     ],
@@ -28,43 +28,43 @@ export const jamRecipes = [
   {
     name: "포도 잼",
     image: GrapeJam,
-    price: 21120,
+    price: 33120,
     ingredients: [{ name: "포도", count: 64, image: getItemImage("포도") }],
   },
   {
     name: "레몬 잼",
     image: LemonJam,
-    price: 21120,
+    price: 33120,
     ingredients: [{ name: "레몬", count: 64, image: getItemImage("레몬") }],
   },
   {
     name: "망고 잼",
     image: MangoJam,
-    price: 28160,
+    price: 44160,
     ingredients: [{ name: "망고", count: 64, image: getItemImage("망고") }],
   },
   {
     name: "멜론 잼",
     image: MelonJam,
-    price: 14080,
+    price: 22080,
     ingredients: [{ name: "멜론", count: 64, image: getItemImage("멜론") }],
   },
   {
     name: "오렌지 잼",
     image: OrangeJam,
-    price: 35200,
+    price: 55200,
     ingredients: [{ name: "오렌지", count: 64, image: getItemImage("오렌지") }],
   },
   {
     name: "복숭아 잼",
     image: PeachJam,
-    price: 35200,
+    price: 55200,
     ingredients: [{ name: "복숭아", count: 64, image: getItemImage("복숭아") }],
   },
   {
     name: "파인애플 잼",
     image: PineappleJam,
-    price: 21120,
+    price: 33120,
     ingredients: [
       { name: "파인애플", count: 64, image: getItemImage("파인애플") },
     ],
@@ -72,7 +72,7 @@ export const jamRecipes = [
   {
     name: "딸기 잼",
     image: StrawberryJam,
-    price: 14080,
+    price: 22080,
     ingredients: [{ name: "딸기", count: 64, image: getItemImage("딸기") }],
   },
 ];
--- src/constants/food/wine.js
+++ src/constants/food/wine.js
@@ -14,13 +14,13 @@ export const wineRecipes = [
   {
     name: "바나나 와인",
     image: BananaWine,
-    price: 23040,
+    price: 37440,
     ingredients: [{ name: "바나나", count: 64, image: getItemImage("바나나") }],
   },
   {
     name: "블루베리 와인",
     image: BlueberryWine,
-    price: 15360,
+    price: 24960,
     ingredients: [
       { name: "블루베리", count: 64, image: getItemImage("블루베리") },
     ],
@@ -28,43 +28,43 @@ export const wineRecipes = [
   {
     name: "포도 와인",
     image: GrapeWine,
-    price: 23040,
+    price: 37440,
     ingredients: [{ name: "포도", count: 64, image: getItemImage("포도") }],
   },
   {
     name: "레몬 와인",
     image: LemonWine,
-    price: 23040,
+    price: 37440,
     ingredients: [{ name: "레몬", count: 64, image: getItemImage("레몬") }],
   },
   {
     name: "망고 와인",
     image: MangoWine,
-    price: 30720,
+    price: 49920,
     ingredients: [{ name: "망고", count: 64, image: getItemImage("망고") }],
   },
   {
     name: "멜론 와인",
     image: MelonWine,
-    price: 15360,
+    price: 24960,
     ingredients: [{ name: "멜론", count: 64, image: getItemImage("멜론") }],
   },
   {
     name: "오렌지 와인",
     image: OrangeWine,
-    price: 38400,
+    price: 62400,
     ingredients: [{ name: "오렌지", count: 64, image: getItemImage("오렌지") }],
   },
   {
     name: "복숭아 와인",
     image: PeachWine,
-    price: 38400,
+    price: 62400,
     ingredients: [{ name: "복숭아", count: 64, image: getItemImage("복숭아") }],
   },
   {
     name: "파인애플 와인",
     image: PineappleWine,
-    price: 23040,
+    price: 37440,
     ingredients: [
       { name: "파인애플", count: 64, image: getItemImage("파인애플") },
     ],
@@ -72,7 +72,7 @@ export const wineRecipes = [
   {
     name: "딸기 와인",
     image: StrawberryWine,
-    price: 15360,
+    price: 24960,
     ingredients: [{ name: "딸기", count: 64, image: getItemImage("딸기") }],
   },
 ];
